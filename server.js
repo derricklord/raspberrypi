48edd0a8e55dfcd257413507381546fd7d4a7d4d
derricklord <derrick.lord@gmail.com>
2015-06-26 20:50:56 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var port = 3030;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/env', function(req,res){
+	var environment = process.env;
+	if(environment){
+		res.json({data: environment});
+	}else{
+		res.send({data: 'No Environment present'});
+	}
+});
+
+app.listen(port, function(){
+	console.log('User Information: ' + process.env.USER);
+	console.log('Server listening on port ' + port);
+});
+
+
